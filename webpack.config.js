d9725cd86e4f7ce3a335cb5ed35895cee376c8d4
yunyoungkil <yunyoungkil76@gmail.com>
2025-11-28 15:13:16 +0900

    feat: dist/editor.bundle.js로 스크립트 경로 변경 및 webpack 설정 수정

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
   entry: {
     content: './content.js',
     background: './background.js',
-    offscreen: './offscreen.js'
+    offscreen: './offscreen.js',
+    editor: './editor.js'
   },
   output: {
     path: path.resolve(__dirname, "dist"),
