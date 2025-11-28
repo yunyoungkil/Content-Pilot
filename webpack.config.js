39dd7103e74c2cf70c5dd461a190b36ea14fde0a
yunyoungkil <yunyoungkil76@gmail.com>
2025-11-28 15:13:36 +0900

    style: 코드 스타일 통일성 개선 및 불필요한 공백 제거

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,20 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: {
-    content: './content.js',
-    background: './background.js',
-    offscreen: './offscreen.js',
-    editor: './editor.js'
+    content: "./content.js",
+    background: "./background.js",
+    offscreen: "./offscreen.js",
+    editor: "./editor.js",
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: '[name].bundle.js',
-    publicPath: '', // Chrome Extension에서는 상대 경로 사용
-    chunkFilename: '[name].bundle.js', // 청크 파일명 명시
+    filename: "[name].bundle.js",
+    publicPath: "", // Chrome Extension에서는 상대 경로 사용
+    chunkFilename: "[name].bundle.js", // 청크 파일명 명시
   },
   optimization: {
     splitChunks: {
-      chunks: 'async', // 동기 import는 스플리팅하지 않음
+      chunks: "async", // 동기 import는 스플리팅하지 않음
       minSize: 20000, // 최소 크기를 크게 설정하여 스플리팅 방지
     },
   },
